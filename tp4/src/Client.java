482aac845f955f04f227290a23c6a384628b526c
irachi <irachi.ali@gmail.com>
2019-10-17 09:37:03 +0200

    add IdVol

--- tp4/src/Client.java
+++ tp4/src/Client.java
@@ -5,9 +5,9 @@ public class Client
     private String nom;
     private String paiement;
     private String contact;
-    private String reference;
+    private IdClient reference;
 
-    public Client(String nom_, String paiement_, String contact_, String ref_)
+    public Client(String nom_, String paiement_, String contact_, IdClient ref_)
     {
         this.nom = nom_;
         this.paiement = paiement_;
